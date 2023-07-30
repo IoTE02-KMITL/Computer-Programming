1b61182ebb6acb7292d6ff634cd411978bdffcd2
Disezmike <mikenatchapon@gmail.com>
2023-07-30 10:10:08 +0700

    📝 docs(lab1): add code and readme files for lab1 exercises
    ✨ feat(01.md): add code and image for Chapter 1, item 1 exercise
    ✨ feat(02.md): add code and image for Chapter 1, item 2 exercise
    ✨ feat(03.md): add code and image for Chapter 1, item 3 exercise
    ✨ feat(04.md): add code and image for Chapter 1, item 4 exercise
    ✨ feat(05.md): add code and image for Chapter 1, item 5 exercise
    ✨ feat(readme.md): add shortcuts to the exercises in lab1
    
    🔧 chore(01.c): add code for Chapter 1, item 1 exercise
    🔧 chore(02.c): add code for Chapter 1, item 2 exercise
    🔧 chore(03.c): add code for Chapter 1, item 3 exercise
    🔧 chore(04.c): add code for Chapter 1, item 4 exercise
    🔧 chore(05.c): add code for Chapter 1, item 5 exercise

--- Computer Programming/lab1/src/01.c
+++ Computer Programming/lab1/src/01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    printf("Hello, Thailand!\n");
     return 0;
 }
---
+++ Computer Programming/lab1/src/04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+    printf("*	*	*	*	*	*	*	*	*	*\n");
+    printf("Hello, KMITL!\n");
+    printf("Hello, Ladkrabang.\n");
+    printf("Hello, Computer Programming.\n");
+    printf("one\ttwo\tthree\tfour\tfive\tHallelujah\n");
+    return 0;
+}
