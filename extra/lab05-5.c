9ac7a5560db1fc64f43c057491f80035a9caaaa8
Disezmike <mikenatchapon@gmail.com>
2023-09-14 01:43:09 +0700

    🐛 fix(lab05-5.c): adjust formatting of printed numbers to align properly and improve readability

--- extra/lab05-5.c
+++ extra/lab05-5.c
@@ -7,7 +7,9 @@ int main() {
     scanf("%[^\n]", input);
 
     for (int i=0; input[i] != '\0'; i++) {
-        printf("%d ", input[i]);
+        if (i == 0 && input[i] / 100 > 0) printf("%-5d", input[i]);
+        else if ((i == 0 && input[i] / 10 > 0) || input[i] / 100 > 0) printf("%-4d", input[i]);
+        else printf("%-3d", input[i]);
         if (input[i+1] != '\0') printf("+ ");
         sum += input[i];
     }
