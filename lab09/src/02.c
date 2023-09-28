0c6e5acc7aa544da3a727cbc40fdf1ff38859fa1
Disezmike <mikenatchapon@gmail.com>
2023-09-28 11:07:58 +0700

    ✨ feat(01.c): add new file 01.c to implement the function add_money_to_wallet and main function to add money to wallet and print the wallet value ✨ feat(02.c): add new file 02.c to implement the function swap_wallet and main function to swap the values of two wallets and print the updated values ✨ feat(03.c): add new file 03.c to implement the function sort and main function to sort an array of scores in descending order and print the sorted array ✨ feat(04.c): add new file 04.c to implement the function mongkol and main function to replace all vowels in a name with 'a' or 'A' and print the modified name ✨ feat(05.c): add new file 05.c to implement the function add_word and main function to add a word to a text and print the modified text ✨ feat(label.txt): add new file label.txt to store labels for each item in chapter 9

---
+++ lab09/src/02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int swap_wallet(int *my_wallet, int *other_wallet);
+
+int main()
+{
+    int my_wallet, other_wallet;
+    printf("Enter my money, other money : ");
+    scanf("%d %d", &my_wallet, &other_wallet);
+    printf("Me : %4d | Other : %4d", my_wallet, other_wallet);
+    swap_wallet(&my_wallet, &other_wallet);
+    printf("\n\n====== Whooop! ======\n\n");
+    printf("Me : %4d | Other : %4d", my_wallet, other_wallet);
+}
+
+int swap_wallet(int *my_wallet, int *other_wallet) {
+    int temp = *my_wallet;
+    *my_wallet = *other_wallet;
+    *other_wallet = temp;
+}
